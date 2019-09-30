83fea892c12fd86c96158dafee41b150b608020b
曹祖鹏 <chinalibra@163.com>
2019-09-30 08:46:55 +0800

    增加换行符

--- hello.go
+++ hello.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println(quote.Hello())
 	var list, _ = discovery.Run("hci0", false)
 	for _, pro := range list {
-		fmt.Printf("获取到设备名%s 设备地址%s ,设备类型%s, 强度%d",
+		fmt.Printf("获取到设备名%s 设备地址%s ,设备类型%s, 强度%d \n",
 			pro.Name, pro.Address, pro.AddressType, pro.RSSI)
 	}
 }
